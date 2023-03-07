f0f76a0f474c12e535fd483b2bf9ced0d5fc89f8
yourangel4 <moyinoluwa20@gmail.com>
2023-03-07 12:35:58 -0800

    100 print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# Author - Moyinoluwa oluyeye
+# 100-print_tebahpla.py
 
+"""Print the aplhabet in reverse order alternating upper- and lower-case."""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
